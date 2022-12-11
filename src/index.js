e9ad83e5e198af38038a9e996236ad75bbb17ec2
acvarun <96919265+acvarun@users.noreply.github.com>
2022-12-11 18:19:01 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
+const root= ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
+);
+
 
 
